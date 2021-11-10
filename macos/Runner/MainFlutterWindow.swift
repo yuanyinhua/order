be7429e091b31b6b77fa5c5bac68a877cd10dde2
yuanyinhua <627910941@qq.com>
2021-11-10 09:09:09 +0800

    提交

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,14 +4,10 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
-    var windowFrame = self.frame
-    windowFrame.size.height = 420
-    windowFrame.size.width = 350
+    let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-    self.setContentSize(NSSize(width: 350,height: 420))
-    let window: NSWindow! = self.contentView?.window
-    window.styleMask.remove(.resizable)
+
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
